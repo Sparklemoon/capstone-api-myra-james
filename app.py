c106001a52b4271fa3d666429c587dc546c320a4
Myra James <Treesmellgood@gmail.com>
2021-01-05 15:38:21 -0500

    cors

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy   
-from flask_marshmallow import Marshmallow  
-from flask_cors import CORS  
+from flask_marshmallow import Marshmallow    
 from flask_heroku import Heroku
 from flask_bcrypt import Bcrypt  
 
